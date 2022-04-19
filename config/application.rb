1aaeb677d17122a60b87b148b3d09c5b6ec697da
robertDurst <me@robdurst.com>
2022-04-18 20:55:21 -0400

    turn on sot by default again

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module YourProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.is_sot = false
+    config.is_sot = true
 
     # Configuration for the application, engines, and railties goes here.
     #
